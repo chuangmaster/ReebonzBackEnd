b269bd97d20b17550c76b26fda7481a73126696b
chuangmaster <chuang.master@gmail.com>
2018-11-14 22:38:49 +0800

    調整RedundAddDTOParameter屬性

--- Reebonz.Service/DTO/Parameters/RefundAddDTOParameter.cs
+++ Reebonz.Service/DTO/Parameters/RefundAddDTOParameter.cs
@@ -6,8 +6,49 @@ using System.Threading.Tasks;
 
 namespace Reebonz.Service.DTO.Parameters
 {
-
+    /// <summary>
+    /// class RefundAddDTOParameter
+    /// </summary>
     public class RefundAddDTOParameter
     {
+        /// <summary>
+        /// 通知物流時間
+        /// </summary>
+        public DateTime ShipmentDate { get; set; }
+
+        /// <summary>
+        /// 客戶要求取件時間
+        /// </summary>
+        public DateTime AskDate { get; set; }
+
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string Memo { get; set; }
+
+        /// <summary>
+        /// 退款金額
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 資料編輯者ID
+        /// </summary>
+        public int UNo { get; set; }
+
+        /// <summary>
+        /// Case Number
+        /// </summary>
+        public string CaseNum { get; set; }
+
+        /// <summary>
+        /// 資料寫入時間
+        /// </summary>
+        public DateTime DateIn { get; set; }
+
+        /// <summary>
+        /// 資料修改時間
+        /// </summary>
+        public DateTime ModifiedTime { get; set; }
     }
 }
