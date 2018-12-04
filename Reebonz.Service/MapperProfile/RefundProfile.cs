0e4bc3aa2034430916e14689a3e68994bd892ef8
chuangmaster <chuang.master@gmail.com>
2018-12-05 00:53:38 +0800

    加入AutoMapper Profile項目；升級AutoMapper版本

---
+++ Reebonz.Service/MapperProfile/RefundProfile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Reebonz.Dapper.Repository.Models;
+using Reebonz.Service.DTO;
+
+namespace Reebonz.Service.MapperProfile
+{
+    /// <summary>
+    /// class RefundProfile
+    /// </summary>
+    internal class RefundProfile : Profile
+    {
+        internal static IMapper Config = new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<RefundModel, RefundDTO>();
+        }).CreateMapper();
+    }
+}
