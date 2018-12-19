590fb2b798c33578925424569ed864046a28a0b7
chuangmaster <chuang.master@gmail.com>
2018-12-20 00:20:34 +0800

    加入OutputModel；加入ParameterModel

---
+++ Reebonz/Models/Output/ResponseResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reebonz.Models.Output
+{
+    /// <summary>
+    /// class ResponseResult
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ResponseResult<T>
+    {
+        /// <summary>
+        /// 資料
+        /// </summary>
+        public T Data { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 訊息
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
---
+++ Reebonz/Models/Parameters/RefundAddActionParameter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reebonz.Models.Parameters
+{
+    /// <summary>
+    /// class RefundAddActionParameter
+    /// </summary>
+    public class RefundAddActionParameter
+    {
+        /// <summary>
+        /// 通知物流時間
+        /// </summary>
+        public DateTime ShipmentDate { get; set; }
+
+        /// <summary>
+        /// 客戶要求取件時間
+        /// </summary>
+        public DateTime AskDate { get; set; }
+
+        /// <summary>
+        /// 備註
+        /// </summary>
+        public string Memo { get; set; }
+
+        /// <summary>
+        /// 退款金額
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// 資料編輯者ID
+        /// </summary>
+        public int UNo { get; set; }
+
+        /// <summary>
+        /// Case Number
+        /// </summary>
+        public string CaseNum { get; set; }
+    }
+}
