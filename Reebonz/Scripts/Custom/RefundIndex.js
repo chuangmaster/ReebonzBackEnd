bb3d40984ebb3746be7dc781baab9bd9388446ef
chuangmaster <chuang.master@gmail.com>
2018-12-20 00:21:03 +0800

    加入RefundIndex.js

---
+++ Reebonz/Scripts/Custom/RefundIndex.js
@@ -0,0 +1,24 @@
+﻿var app = new Vue({
+    el: '#app',
+    data: {
+        transationID: ''
+    },
+    methods: {
+        searchTransationID: function () {
+            alert('!');
+            $.ajax({
+                url: '/api/v1/Order/' + this.transationID,
+                success: function (data, xhrObj, ) {
+                    alert('success');
+                },
+                error: function () {
+                    alert('error');
+                }
+            });
+        },
+        test: function () {
+
+        }
+    }
+
+});
